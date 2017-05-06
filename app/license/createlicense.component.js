613cc630f2fd663b8a0dd834820932ad01187123
刘盾 <liudun@liudundeMacBook-Pro.local>
2017-05-06 17:57:33 +0800

    jijdisajiLines starting

--- app/license/createlicense.component.js
+++ app/license/createlicense.component.js
@@ -6,8 +6,8 @@ module('createlicense', ['service', 'service.user', 'ui.bootstrap', 'ui.bootstra
         templateUrl: 'license/createlicense.template.html',
         //三个DT derective的引入次序必须在$q之前而且有先后顺序,否则会报错
         //数组前几个元素为provider                                                           //再次声明,上下两行的对应关系一定要严格执行,先后次序也不能错
-        controller: ['$q', '$scope', '$compile', '$http', '$sce', 'urlconfig', '$route',
-            function licenseController($q, $scope, $compile, $http, $sce, urlconfig, $route) {
+        controller: ['$q', '$scope', '$compile', '$http', '$sce', 'urlconfig', '$route','$location',
+            function licenseController($q, $scope, $compile, $http, $sce, urlconfig, $route,$location) {
 
                 var self = this;
                 var defer = $q.defer();
